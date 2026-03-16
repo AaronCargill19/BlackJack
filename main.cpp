51df839e3f7f30fb0053d0df983316f1887d6786
AaronCargill19 <abcargillpdx@gmail.com>
2026-03-16 13:13:02 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+
+
+class Card {
+
+private:
+    bool isFaceUp;
+
+    public:
+    std::string RANK; //A,1,2,3,4,5,6,7,8,9,10,J,Q,K
+    char SUIT;
+
+    Card(std::string rank = "A", char suit = 'S', bool isFU = true) {
+        RANK = rank;
+        SUIT = suit;
+        isFaceUp = isFU;
+    }
+
+    int getValue() {
+        int value = 0;
+        if (!isFaceUp) {
+            return value;
+        }
+        //try to convert to int value first
+        try {
+            value = std::stoi(RANK);
+        }
+        catch (std::invalid_argument) {
+            if (RANK == "A") {
+                value = 1;
+            }
+            else if (RANK == "J" || RANK == "Q" || RANK == "K") {
+                value = 10;
+            }
+        }
+        return value;
+    }
+
+    void flip() {
+        if (isFaceUp) {
+            isFaceUp = false;
+        }
+        else {
+            isFaceUp = true;
+        }
+    }
+
+};
+
+
+class Hand {
+
+public:
+    std::vector<Card*> cardVector;
+
+    Hand() {
+        cardVector.reserve(7);
+    }
+
+    ~Hand() {
+        clearHand();
+    }
+
+    void addCard(const Card& card) {
+        cardVector.push_back(new Card(card));
+    }
+
+    void clearHand() {
+        std::vector<Card*>::iterator iter;
+
+        for (iter = cardVector.begin(); iter != cardVector.end(); ++iter) {
+            delete *iter;
+            *iter = NULL;
+        }
+        cardVector.clear();
+    }
+
+    int getTotal() {
+
+        int total = 0;
+        bool hasAce = false;
+
+        for (std::vector<Card*>::iterator iter = cardVector.begin(); iter != cardVector.end(); ++iter) {
+            if (*iter != NULL) {
+                total += (*iter)->getValue();
+                if ((*iter)->RANK == "A") {
+                    hasAce = true;
+                }
+            }
+        }
+        if (hasAce && total <= 11) {
+            total += 10;
+        }
+
+        return total;
+    }
+
+};
+
+
+class GenericPlayer {
+
+public:
+
+    std::string playerName;
+    Hand playerHand;
+
+    GenericPlayer(std::string playerName) {
+        playerName = playerName;
+    }
+
+    bool isBusted() {
+        if (playerHand.getTotal() > 21) {
+            return true;
+        }
+        return false;
+    }
+
+    void bust() {
+        std::cout << playerName << " is Busted!"<< std::endl;
+    }
+
+};
+
+
+class Player : public GenericPlayer {
+
+    public:
+    Player(std::string n) : GenericPlayer(n) {}
+
+    void isHitting(Card card) {
+        if (!isBusted()) {
+            playerHand.addCard(card);
+        }
+        else {
+            bust();
+        }
+    }
+
+    void win() {
+        std::cout << playerName << " has won!" << std::endl;
+    }
+
+    void lose() {
+        std::cout << playerName << " has lost!" << std::endl;
+    }
+
+    void push() {
+        std::cout << playerName << " has pushed!" << std::endl;
+    }
+
+};
+
+
+int main(){
+    Card aceSpades = Card("A", 'S');
+    Card tenHearts = Card("10", 'H');
+    Hand hand;
+    hand.addCard(aceSpades);
+    hand.addCard(tenHearts);
+    std::cout << hand.getTotal() << std::endl;
+}
